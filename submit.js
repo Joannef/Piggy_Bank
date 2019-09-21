fbae2fcf5c8a3415fe14e088f3a6bf108f20a5aa
Steven C <svchan999@gmail.com>
2019-09-21 09:50:23 -0400

    finished calculations

--- submit.js
+++ submit.js
@@ -3,7 +3,7 @@ function howMuchDoYouWantToSaveBeforeDeductibles() {
 	const pay = document.getElementById("check").value //this is how much a person gets per paycheck
 	const goal = document.getElementById("g_amount").value //this is how much you want to save up
 	const save = document.getElementById("save").value //how much you want to save per paycheck
-	const howmanychecks = (pay - goal) / save //how many paychecks until goal is reached
+	const howmanychecks = Math.abs((pay - goal))/ save //how many paychecks until goal is reached
 	document.getElementById("howlong").innerHTML = howmanychecks + " paychecks";
 	//console.log(howmanychecks);
 }
@@ -18,38 +18,50 @@ function howMuchDoYouWantToSaveBeforeDeductibles() {
 // }
 
 function howMuchDeductiblesPerX() {
+	const pay = document.getElementById("check").value //this is how much a person gets per paycheck
+	const save = document.getElementById("save").value //how much you want to save per paycheck
     const typedate = document.getElementById("dateType").value //this is how often I select my expenses to be paid
     const checkType = document.getElementById("checkType").value //this is how often I get payed
+    var total;
     console.log(typedate);
     console.log(checkType);
     let totaldeductions = document.getElementById("deductibles").value
     console.log(totaldeductions + " before if statements");
     if(checkType == "weekly") {
-    	if(typedate == "week") 
-    		console.log(totaldeductions + " NO OPERATIONS");
+    	if(typedate == "week") {
+    		total = pay-totaldeductions-save;
+    		console.log(total);
+    	}
     	else if(typedate == "month") {
     		totaldeductions = totaldeductions / 4;
-    		console.log(totaldeductions + " BREAK");
+    		total = pay-totaldeductions-save;
+    		console.log(total);
     	}
     }
 
     if(checkType == "bi-weekly") {
     	if(typedate == "week") {
     		totaldeductions = totaldeductions * 2;
-    		console.log(totaldeductions);
+    		total = pay-totaldeductions-save;
+    		console.log(total);
     	}
     	else if(typedate == "month") {
     		totaldeductions = totaldeductions / 2;
-    		console.log(totaldeductions);
+    		total = pay-totaldeductions-save;
+    		console.log(total);
     	}
     }
 
     if(checkType == "monthly") {
     	if(typedate == "week") {
     		totaldeductions = totaldeductions * 4;
-    		console.log(totaldeductions);
+    		total = pay-totaldeductions-save;
+    		console.log(total);
     	}
-    	if(typedate == "month")
-    		console.log(totaldeductions + " NO OPERATIONS");
+    	if(typedate == "month") {
+    		total = pay-totaldeductions-save;
+    		console.log(total);
+    	}
     }
+
 }
