2c4d96354a0912e20d6b6220cf9572062e6cbd2e
Joanne <joannef3188@gmail.com>
2019-09-21 11:57:30 -0400

    new colors

--- submit.js
+++ submit.js
@@ -3,10 +3,18 @@ function howMuchDoYouWantToSaveBeforeDeductibles() {
 	const pay = document.getElementById("check").value //this is how much a person gets per paycheck
 	const goal = document.getElementById("g_amount").value //this is how much you want to save up
 	const save = document.getElementById("save").value //how much you want to save per paycheck
-	const howmanychecks = Math.abs((pay - goal))/ save //how many paychecks until goal is reached
+	const howmanychecks = Math.abs((pay - goal)) / save //how many paychecks until goal is reached
 	document.getElementById("howlong").innerHTML = howmanychecks + " paychecks";
+	window.location.href = "outputpage.html";
 	//console.log(howmanychecks);
 }
+
+input.addEventListener("keyup", function (event) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		document.getElementById("button").click();
+	}
+});
 
 // //How much deductions depending on day, week, or month
 // function howMuchDeductiblesPerX() {
@@ -20,48 +28,48 @@ function howMuchDoYouWantToSaveBeforeDeductibles() {
 function howMuchDeductiblesPerX() {
 	const pay = document.getElementById("check").value //this is how much a person gets per paycheck
 	const save = document.getElementById("save").value //how much you want to save per paycheck
-    const typedate = document.getElementById("dateType").value //this is how often I select my expenses to be paid
-    const checkType = document.getElementById("checkType").value //this is how often I get payed
-    var total;
-    console.log(typedate);
-    console.log(checkType);
-    let totaldeductions = document.getElementById("deductibles").value
-    console.log(totaldeductions + " before if statements");
-    if(checkType == "weekly") {
-    	if(typedate == "week") {
-    		total = pay-totaldeductions-save;
-    		console.log(total);
-    	}
-    	else if(typedate == "month") {
-    		totaldeductions = totaldeductions / 4;
-    		total = pay-totaldeductions-save;
-    		console.log(total);
-    	}
-    }
+	const typedate = document.getElementById("dateType").value //this is how often I select my expenses to be paid
+	const checkType = document.getElementById("checkType").value //this is how often I get payed
+	var total;
+	console.log(typedate);
+	console.log(checkType);
+	let totaldeductions = document.getElementById("deductibles").value
+	console.log(totaldeductions + " before if statements");
+	if (checkType == "weekly") {
+		if (typedate == "week") {
+			total = pay - totaldeductions - save;
+			console.log(total);
+		}
+		else if (typedate == "month") {
+			totaldeductions = totaldeductions / 4;
+			total = pay - totaldeductions - save;
+			console.log(total);
+		}
+	}
 
-    if(checkType == "bi-weekly") {
-    	if(typedate == "week") {
-    		totaldeductions = totaldeductions * 2;
-    		total = pay-totaldeductions-save;
-    		console.log(total);
-    	}
-    	else if(typedate == "month") {
-    		totaldeductions = totaldeductions / 2;
-    		total = pay-totaldeductions-save;
-    		console.log(total);
-    	}
-    }
+	if (checkType == "bi-weekly") {
+		if (typedate == "week") {
+			totaldeductions = totaldeductions * 2;
+			total = pay - totaldeductions - save;
+			console.log(total);
+		}
+		else if (typedate == "month") {
+			totaldeductions = totaldeductions / 2;
+			total = pay - totaldeductions - save;
+			console.log(total);
+		}
+	}
 
-    if(checkType == "monthly") {
-    	if(typedate == "week") {
-    		totaldeductions = totaldeductions * 4;
-    		total = pay-totaldeductions-save;
-    		console.log(total);
-    	}
-    	if(typedate == "month") {
-    		total = pay-totaldeductions-save;
-    		console.log(total);
-    	}
-    }
+	if (checkType == "monthly") {
+		if (typedate == "week") {
+			totaldeductions = totaldeductions * 4;
+			total = pay - totaldeductions - save;
+			console.log(total);
+		}
+		if (typedate == "month") {
+			total = pay - totaldeductions - save;
+			console.log(total);
+		}
+	}
 
 }
